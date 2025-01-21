f2f707e7af000cdc887dcd1a83d3269356f36502
aapozd <aapozdnyakove@gmail.com>
2025-01-21 19:06:34 +0000

    Fix vite phaser paths
    Signed-off-by: aapozd <aapozdnyakove@gmail.com>

--- vite.config.js
+++ vite.config.js
@@ -1,6 +1,14 @@
 import { defineConfig } from 'vite';
 
 export default defineConfig({
+  optimizeDeps: {
+    include: ['phaser'], // Ensure Phaser is included in the optimized dependencies
+  },
+  resolve: {
+    alias: {
+      phaser: 'phaser/dist/phaser.js', // Use Phaser's ES module version
+    },
+  },
   server: {
     port: 1234,
   },
